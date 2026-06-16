b6833e10422ab90a96f204165b190bd1c0f8680f
atit <atittulachan6072@gmail.com>
2026-06-16 16:18:14 +0545

    add: js variables

---
+++ 1.variable.js
@@ -0,0 +1,43 @@
+// console.log("Hello World!");
+
+//! dynamic typed, interpreted and single threaded language
+
+//todo variables
+
+//? variable declaration methods
+//? var, let and const
+
+// var
+
+var a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+var a = 'bs';
+console.log(a);
+
+// let
+
+let b= 10;
+console.log(b);
+
+b = 20;
+console.log(b);
+
+b = true;
+console.log(b);
+
+// let a = 30; //? SyntaxError: Identifier 'a' has already been declared
+
+// const
+
+const c = 10; // we cannot reassign a value to a constant variable but we can change the value of a constant variable if it is an object or an array i.e mutable data types
+console.log(c);
+
+// c = 20; //? TypeError: Assignment to constant variable.
+
+//? const d;  SyntaxError: Missing initializer in const declaration
+
+const d ="";
